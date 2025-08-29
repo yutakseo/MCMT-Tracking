cb9b32279ae69459259b7f188415c7024a21b0eb
Yutak Seo <yts@smartinside.ai>
2025-08-29 01:10:51 +0000

    streaming module developing

---
+++ camera.py
@@ -0,0 +1,42 @@
+from __Detection.detection_api import DetectionAPI
+from __Tracking.tracking_api import TrackerAPI
+from homo_graphy import PlanProjector
+import cv2
+
+class Args:
+    track_thresh = 0.5
+    match_thresh = 0.5
+    track_buffer = 60
+    mot20 = False
+    cpu_workers = 10  
+    chunk_sec   = 10.0
+
+class Camera:
+    def __init__(self, cctv_url:str, cord_plan:str, calibration:list,):
+        self.url = cctv_url
+        self.plan_path = cord_plan
+        self.calibration = calibration
+        
+        self.detector = DetectionAPI()
+        self.tracker = TrackerAPI(args=Args(), detector=self.detector)
+        return None
+
+    def webcam_show(self, show:bool):
+        cap = cv2.VideoCapture(0)
+        
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            cv2.imshow("Webcam", frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):  # q 누르면 종료
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+        return None
+    
+    def stream(self, show:bool):
+        
+        return None
