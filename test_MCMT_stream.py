8d3392977497184a4ebba8987bf2927d1943c4fa
Yutak Seo <yts@smartinside.ai>
2025-09-04 12:48:38 +0900

    streaming experiment2 started

--- test_MCMT_stream.py
+++ test_MCMT_stream.py
@@ -23,7 +23,7 @@ os.environ.setdefault(
 # ─────────────────────────────────────────────────────────────────────────────
 # 구성
 # ─────────────────────────────────────────────────────────────────────────────
-PLAN_PATH = "/workspace/assets/250903_homograph_coordinate-plane2.jpg"
+PLAN_PATH = "/workspace/assets/250904_homograph_coordinate-plane2.jpg"
 RTSP1_URL  = "rtsp://admin:asdf2510$_@192.168.50.131:554/video1"
 RTSP2_URL  = "rtsp://admin:asdf2510$_@192.168.50.131:554/video2"
 RTSP3_URL  = "rtsp://admin:asdf2510$_@192.168.50.131:554/video3"
@@ -38,20 +38,46 @@ class Args:
 def build_cameras():
     args = Args()
     plan_pts  = [
-                ( 792,  504),    #point1
-                ( 792,  871),   #point2
-                ( 792, 1250),   #point3
-                (2112,  504),    #point4
-                (2112,  871),   #point5
-                (2112, 1250),   #point6
-                (3432,  504),     #point7
-                (3432,  871),    #point8
-                (3432, 1250),    #point9
+                (1170,  214),    #point1
+                (1170,  559),   #point2
+                (1170,  904),   #point3
+                (2212,  214),    #point4
+                (2212,  559),   #point5
+                (2212,  904),   #point6
+                (3255,  214),     #point7
+                (3255,  559),    #point8
+                (3255,  904),    #point9
                 ]
     
-    cam1_pts = [(1390,521),(1618,552),(1784,578),(1112,564),(1434,620),(1709,668),(481,651),(852,809),(1393,1007)]
-    cam2_pts = [(1020,1027),(476,898),(294,826),(1348,790),(1017,772),(813,753),(1437,716),(1213,713),(1058,707)]
-    cam3_pts = [(140,309),(291,315),(398,320),(213,376),(440,377),(588,378),(467,596),(826,521),(965,484)]
+    cam1_pts = [(1033,475),
+                (948,474),
+                (863,473),
+                (1019,527),
+                (890,524),
+                (769,519),
+                (973,667),
+                (741,652),
+                (548,620)]
+    
+    cam2_pts = [(518,466),
+                (430,471),
+                (341,474),
+                (613,510),
+                (485,519),
+                (354,527),
+                (829,608), 
+                (634,645),
+                (397,668)]
+    
+    cam3_pts = [(357,602),
+                (566,648),
+                (832,683),
+                (620,498),
+                (754,509),
+                (893,513),
+                (726,453),
+                (819,456),
+                (911,459)]
 
     return [
         SCST(RTSP1_URL, cam1_pts, PLAN_PATH, plan_pts, args),
@@ -65,7 +91,9 @@ def build_rtsp_streams():
     CCTVStreamer(pybind11)가 있으면 자동 사용, 없으면 OpenCV VideoCapture 폴백.
     """
     streams = {
-        "cam1": CamMJPEG(name="cam1", url=RTSP_URL, width=1080).start()
+        "cam1": CamMJPEG(name="cam1", url=RTSP1_URL, width=1080).start(),
+        "cam2": CamMJPEG(name="cam2", url=RTSP2_URL, width=1080).start(),
+        "cam3": CamMJPEG(name="cam3", url=RTSP3_URL, width=1080).start()
         # 서로 다른 RTSP라면 각각 다른 URL로 지정하면 됩니다.
     }
     return streams
